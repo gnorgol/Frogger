2c8e3db21dfdbbe5a86d434c1672dc0c10abb51a
Guillaume Norgol <52493523+gnorgol@users.noreply.github.com>
2024-11-19 10:58:24 +0100

    Ajout de gestion des sprites et déplacement de Frogger
    - Ajout des propriétés publiques `idleSprite` et `jumpSprite` dans la classe `Frogger` pour gérer les sprites en attente et en saut.
    - Ajout d'une variable privée `spriteRenderer` pour stocker le composant `SpriteRenderer`.
    - Ajout de la méthode `Awake` pour initialiser `spriteRenderer`.
    - Ajout de la méthode `Update` pour gérer les entrées utilisateur et appeler `Move`.
    - Ajout de la méthode `Move` pour démarrer une coroutine de déplacement.
    - Ajout de la coroutine `MoveCoroutine` pour animer le déplacement et changer les sprites.
    - Mise à jour de `SampleScene.unity` pour inclure les nouvelles propriétés dans le composant `MonoBehaviour`.

--- Assets/Scripts/Frogger.cs
+++ Assets/Scripts/Frogger.cs
@@ -4,5 +4,61 @@ using UnityEngine;
 
 public class Frogger : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
+
+    public Sprite idleSprite;
+    public Sprite jumpSprite;
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            Move(Vector3.up);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 180);
+            Move(Vector3.down);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 90);
+            Move(Vector3.left);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, -90);
+            Move(Vector3.right);
+        }
+    }
+    private void Move(Vector3 direction)
+    {
+        //transform.position += direction;
+
+        StartCoroutine(MoveCoroutine(direction));
+    }
+
+    private IEnumerator MoveCoroutine(Vector3 direction)
+    {
+        Vector3 targetPosition = transform.position + direction;
+        float time = 0;
+        float duration = 0.125f;
+
+        spriteRenderer.sprite = jumpSprite;
+
+        while (time < duration)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetPosition, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPosition;
+        spriteRenderer.sprite = idleSprite;
+    }
 
 }
